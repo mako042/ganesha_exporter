a08d5d6aea2341ea965e98c57cb983afb9c87a5b
a.p.matveev <a.p.matveev@tmn4.esoft.tech>
2026-04-27 12:07:33 +0500

    fix export logics

--- dbus/export_mgr.go
+++ dbus/export_mgr.go
@@ -6,7 +6,6 @@ import (
 	"github.com/godbus/dbus"
 )
 
-// Export Structure of the output of ShowExports dbus call
 type Export struct {
 	ExportID uint16
 	Path     string
@@ -20,12 +19,10 @@ type Export struct {
 	}
 }
 
-// ExportMgr is a handle to dbus object ExportMgr
 type ExportMgr struct {
 	dbusObject dbus.BusObject
 }
 
-// NewExportMgr Get a new ExportMgr
 func NewExportMgr() ExportMgr {
 	conn, err := dbus.SystemBus()
 	if err != nil {
@@ -41,20 +38,36 @@ func NewExportMgr() ExportMgr {
 
 func (m ExportMgr) ShowExports() (uint64, []Export) {
 	call := m.dbusObject.Call("org.ganesha.nfsd.exportmgr.ShowExports", 0)
+	if call.Err != nil {
+		log.Panic(call.Err)
+	}
 
 	var raw []interface{}
 	if err := call.Store(&raw); err != nil {
 		log.Panic(err)
 	}
 
-	// raw = [ (tt), array ]
-	header := raw[0].([]interface{})
-	exportsRaw := raw[1].([]interface{})
+	if len(raw) < 2 {
+		log.Panic("invalid DBus response: not enough fields")
+	}
 
-	var exports []Export
+	header, ok := raw[0].([]interface{})
+	if !ok || len(header) == 0 {
+		log.Panic("invalid header format")
+	}
+
+	exportsRaw, ok := raw[1].([]interface{})
+	if !ok {
+		log.Panic("invalid exports format")
+	}
+
+	exports := make([]Export, 0, len(exportsRaw))
 
 	for _, e := range exportsRaw {
-		item := e.([]interface{})
+		item, ok := e.([]interface{})
+		if !ok || len(item) < 5 {
+			continue
+		}
 
 		exp := Export{
 			ExportID:  item[0].(uint16),
@@ -62,117 +75,26 @@ func (m ExportMgr) ShowExports() (uint64, []Export) {
 			Protocols: map[string]bool{},
 		}
 
-		// ((sb)(sb)...)
-		flags := item[2].([]interface{})
+		flags, _ := item[2].([]interface{})
 		for _, f := range flags {
-			pair := f.([]interface{})
-			name := pair[0].(string)
-			val := pair[1].(bool)
+			pair, ok := f.([]interface{})
+			if !ok || len(pair) < 2 {
+				continue
+			}
+			name, _ := pair[0].(string)
+			val, _ := pair[1].(bool)
 			exp.Protocols[name] = val
 		}
 
-		exp.Last = item[3].(uint64)
+		exp.Last, _ = item[3].(uint64)
 
-		ts := item[4].([]interface{})
-		exp.Time.Sec = ts[0].(uint64)
-		exp.Time.Nsec = ts[1].(uint64)
+		if ts, ok := item[4].([]interface{}); ok && len(ts) >= 2 {
+			exp.Time.Sec, _ = ts[0].(uint64)
+			exp.Time.Nsec, _ = ts[1].(uint64)
+		}
 
 		exports = append(exports, exp)
 	}
 
 	return header[0].(uint64), exports
-}
-
-func (mgr ExportMgr) GetNFSv3IO(exportID uint16) BasicStats {
-	out := BasicStats{}
-	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv3IO", 0, exportID)
-	if call.Err != nil {
-		log.Panic(call.Err)
-	}
-	if !call.Body[0].(bool) {
-		if err := call.Store(&out.Status, &out.Error, &out.Time); err != nil {
-			log.Panic(err)
-		}
-		return out
-	}
-	if err := call.Store(
-		&out.Status, &out.Error, &out.Time,
-		&out.Read, &out.Write,
-	); err != nil {
-		log.Panic(err)
-	}
-	return out
-}
-
-func (mgr ExportMgr) GetNFSv40IO(exportID uint16) BasicStats {
-	out := BasicStats{}
-	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv40IO", 0, exportID)
-	if call.Err != nil {
-		log.Panic(call.Err)
-	}
-	if !call.Body[0].(bool) {
-		if err := call.Store(&out.Status, &out.Error, &out.Time); err != nil {
-			log.Panic(err)
-		}
-		return out
-	}
-	if err := call.Store(
-		&out.Status, &out.Error, &out.Time,
-		&out.Read, &out.Write,
-	); err != nil {
-		log.Panic(err)
-	}
-	return out
-}
-
-func (mgr ExportMgr) GetNFSv41IO(exportID uint16) BasicStats {
-	out := BasicStats{}
-	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv41IO", 0, exportID)
-	if call.Err != nil {
-		log.Panic(call.Err)
-	}
-	if !call.Body[0].(bool) {
-		if err := call.Store(&out.Status, &out.Error, &out.Time); err != nil {
-			log.Panic(err)
-		}
-		return out
-	}
-	if Gandi {
-		if err := call.Store(
-			&out.Status, &out.Error, &out.Time,
-			&out.Read, &out.Write,
-			&out.Open, &out.Close, &out.Getattr, &out.Lock,
-		); err != nil {
-			log.Panic(err)
-		}
-	} else {
-		if err := call.Store(
-			&out.Status, &out.Error, &out.Time,
-			&out.Read, &out.Write,
-		); err != nil {
-			log.Panic(err)
-		}
-	}
-	return out
-}
-
-func (mgr ExportMgr) GetNFSv41Layouts(exportID uint16) PNFSOperations {
-	out := PNFSOperations{}
-	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv41Layouts", 0, exportID)
-	if call.Err != nil {
-		log.Panic(call.Err)
-	}
-	if !call.Body[0].(bool) {
-		if err := call.Store(&out.Status, &out.Error, &out.Time); err != nil {
-			log.Panic(err)
-		}
-		return out
-	}
-	if err := call.Store(
-		&out.Status, &out.Error, &out.Time,
-		&out.Getdevinfo, &out.LayoutGet, &out.LayoutCommit, &out.LayoutReturn, &out.LayoutRecall,
-	); err != nil {
-		log.Panic(err)
-	}
-	return out
 }
